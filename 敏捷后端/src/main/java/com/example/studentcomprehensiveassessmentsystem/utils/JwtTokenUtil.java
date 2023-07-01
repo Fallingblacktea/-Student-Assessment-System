58de974b3464f54fb6c45a64d7859d44d3e2e50e
caravelle <bark.out.of.trunk@gmail.com>
2023-07-01 16:36:07 +0800

    expired

--- 敏捷后端/src/main/java/com/example/studentcomprehensiveassessmentsystem/utils/JwtTokenUtil.java
+++ 敏捷后端/src/main/java/com/example/studentcomprehensiveassessmentsystem/utils/JwtTokenUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Component
+@Data
 public class JwtTokenUtil {
 
 
