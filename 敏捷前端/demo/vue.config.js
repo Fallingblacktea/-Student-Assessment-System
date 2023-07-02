9baf16e7726a225ed695c78f09cc436c19ca90d6
xzxiong <1248267019@qq.com>
2023-07-02 23:39:21 +0800

    2339

--- 敏捷前端/demo/vue.config.js
+++ 敏捷前端/demo/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
       proxy: {
         // 设置代理，将请求转发到目标服务器
         '/': {
-          target: 'http://localhost:28080/', // 将目标服务器地址替换为实际的目标服务器地址
+          target: 'http://localhost:28080', // 将目标服务器地址替换为实际的目标服务器地址
           changeOrigin: true,
         }
       }
