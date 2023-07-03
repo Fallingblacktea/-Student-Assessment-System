3254fa46750d489278765e03e2a26a985472c583
caravelle <bark.out.of.trunk@gmail.com>
2023-07-03 11:56:29 +0800

    1

--- 敏捷后端/src/main/java/com/example/studentcomprehensiveassessmentsystem/controller/KaptchaController.java
+++ 敏捷后端/src/main/java/com/example/studentcomprehensiveassessmentsystem/controller/KaptchaController.java
@@ -4,16 +4,18 @@ import com.example.studentcomprehensiveassessmentsystem.service.RedisService;
 import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.google.code.kaptcha.util.Config;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.imageio.ImageIO;
-import javax.servlet.http.Cookie;
+import org.springframework.http.ResponseCookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.time.Duration;
 
 @Controller
 public class KaptchaController {
@@ -35,14 +37,18 @@ public class KaptchaController {
         // 生成验证码图片
         BufferedImage captchaImage = defaultKaptcha.createImage(captchaText);
 
+        ResponseCookie cookie = ResponseCookie.from("SessionID", request.getSession().getId()) // key & value
+                .secure(true)
+                //    .domain("localhost")  // host
+                //    .path("/")      // path
+                .maxAge(Duration.ofHours(1))
+                .sameSite("None")  // sameSite
+                .build()
+                ;
 
-        // 创建一个名为"SessionID"的Cookie
-        Cookie cookie = new Cookie("SessionID", request.getSession().getId());
-        // 设置Cookie的有效期，单位为秒
-        cookie.setMaxAge(60);
-        // 将Cookie添加到响应中
-        response.addCookie(cookie);
-        response.setHeader("set-cookie", "SessionID=" + request.getSession().getId() + "; Path=/");
+        // Response to the client
+        response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
         // 将验证码图片写入响应
         response.setContentType("image/png");
         try (OutputStream outputStream = response.getOutputStream()) {
